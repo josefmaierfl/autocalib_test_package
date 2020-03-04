180254c1c97a6912ca61a8900f2d0d9f45315fe1
Josef Maier <josefmaierfl@users.noreply.github.com>
2020-03-04 21:04:07 +0100

    Added cuda installation in docker file

--- run_docker_base.sh
+++ run_docker_base.sh
@@ -2,6 +2,8 @@
 
 RES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )/results"
 mkdir ${RES_DIR}
+# RES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )/results/results_001/results"
+# OUTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )/python_tests_out"
 RES_SV_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )/res_save_compressed"
 mkdir ${RES_SV_DIR}
 
@@ -21,13 +23,13 @@ fi
 # -c $(echo "${@:2}")
 xhost +local:
 #docker run -v `pwd`/py_test_scripts:/app/py_test_scripts -it -v /tmp/.X11-unix/:/tmp/.X11-unix:ro ac_test_package:1.0 /bin/bash
-#docker run -v `pwd`/images:/app/images:ro -v `pwd`/py_test_scripts:/app/py_test_scripts -v ${RES_DIR}:/app/results -v ${RES_SV_DIR}:/app/res_save_compressed -it -v /tmp/.X11-unix/:/tmp/.X11-unix:ro ac_test_package:1.0 /bin/bash
-docker run -v `pwd`/images:/app/images:ro -v `pwd`/py_test_scripts:/app/py_test_scripts -v ${RES_DIR}:/app/results -v ${RES_SV_DIR}:/app/res_save_compressed -it -v /tmp/.X11-unix/:/tmp/.X11-unix:ro ac_test_package_ngransac:1.0 /app/start_testing.sh "${@:2}"
+# docker run --gpus all -v `pwd`/images:/app/images:ro -v `pwd`/py_test_scripts:/app/py_test_scripts -v ${OUTDIR}:/app/output -v ${RES_DIR}:/app/results -v ${RES_SV_DIR}:/app/res_save_compressed -it -v /tmp/.X11-unix/:/tmp/.X11-unix:ro ac_test_package_ngransac:1.0 /bin/bash
+docker run --gpus all -v `pwd`/images:/app/images:ro -v `pwd`/py_test_scripts:/app/py_test_scripts -v ${RES_DIR}:/app/results -v ${RES_SV_DIR}:/app/res_save_compressed -it -v /tmp/.X11-unix/:/tmp/.X11-unix:ro ac_test_package_ngransac:1.0 /app/start_testing.sh "${@:2}"
 
 # Shut down if asked for
-#if [ $# -ne 0 ]; then
+if [ $# -ne 0 ]; then
     if [ "${FIRST_ARG}" == "shutdown" ]; then
         echo "Shutting down"
         sudo shutdown -h now
     fi
-#fi
+fi
