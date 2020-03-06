065883aa002b9e87bd8c1951b68b52d58dabadb3
Josef Maier <josefmaierfl@users.noreply.github.com>
2020-03-06 10:12:20 +0100

    Added export MKL_THREADING_LAYER=TBB to start script

--- start_testing.sh
+++ start_testing.sh
@@ -4,5 +4,6 @@ PY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/py_test_scripts"
 cd ${PY_DIR}
 eval "$(conda shell.bash hook)"
 conda activate NGRANSAC
+export MKL_THREADING_LAYER=TBB
 #echo "works $1"
 python main.py "$@"
