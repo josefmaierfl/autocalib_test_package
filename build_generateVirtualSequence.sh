a4adafc182b218cfa1db1b4861c3996f18136057
Josef Maier <josefmaierfl@users.noreply.github.com>
2020-06-30 12:40:48 +0200

    A fix

--- build_generateVirtualSequence.sh
+++ build_generateVirtualSequence.sh
@@ -13,6 +13,8 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-copy_dir=${root_dir}/tmp/generateVirtualSequence/build
+copy_dir0=${root_dir}/tmp/generateVirtualSequence
+copy_dir=${copy_dir0}/build
 mkdir -p ${copy_dir}
 find ${build_dir} -type f \( -executable -o -name \*.so -name \*.py \) -exec cp {} ${copy_dir} \;
+find ${virtsequ_dir} -type f \( -name \*.py \) -exec cp {} ${copy_dir0} \;
