c50cb6ad2593f923a86675530132ee77f10a421d
Josef Maier <josefmaierfl@users.noreply.github.com>
2020-07-01 19:18:00 +0200

    Some changes

--- start_training.sh
+++ start_training.sh
@@ -2,8 +2,12 @@
 
 PY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/ngransac_train"
 cd ${PY_DIR}
+NGRANSAC_FILE="${PY_DIR}/dataset.py"
+if [ !(-s ${NGRANSAC_FILE}) ]; then
+  ./prepare_workspace.sh
+fi
 eval "$(conda shell.bash hook)"
 conda activate NGRANSAC
 export MKL_THREADING_LAYER=TBB
 #echo "works $1"
-python main.py "$@"
+python main_train.py "$@"
