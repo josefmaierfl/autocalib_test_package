4af39d6a118a2ee480347ae9dbebde0712c5be2f
Josef Maier <josefmaierfl@users.noreply.github.com>
2020-08-28 14:19:07 +0200

    Added inlier ratio and reprojection error statistic calculation

--- ngransac_train/extract_virtSequ_high_inlrat.py
+++ ngransac_train/extract_virtSequ_high_inlrat.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os, sys
 from shutil import copy
+import statistics
 
 from dataset_notorch_virtSequ import SparseDataset
 import util_virtSequ
@@ -18,6 +19,9 @@ parser.add_argument('--path_out', '-po',
 
 parser.add_argument('--variant', '-v', default='train', choices=['train', 'validate'],
 	help='subfolder of the dataset to use')
+
+parser.add_argument('--minInlRat', '-irt', type=float, default=0.29,
+                        help='Threshold on the inlier ratio to take only ratios above given ratio')
 
 opt = parser.parse_args()
 
@@ -40,6 +44,48 @@ print('Starting ...\n')
 
 testset = SparseDataset(data_folder, opt.threshold)
 
+high = []
+low = []
+errm = []
 for filename, pts1, pts2, gt_F, gt_R, gt_t, K1, K2, gt_res, gt_inliers, inl_cnt, inlrat in testset:
-	if inlrat > 0.49:
+	if inlrat > opt.minInlRat:
 		copy(filename, out_folder)
+		high.append(inlrat)
+		err = np.array(gt_res)
+		err = err[gt_inliers]
+		errm.append(np.mean(err))
+	else:
+		low.append(inlrat)
+
+mean_h = statistics.mean(high)
+mean_l = statistics.mean(low)
+# med_h = statistics.median(high)
+# med_l = statistics.median(low)
+sd_h = statistics.stdev(high)
+sd_l = statistics.stdev(low)
+mi_h = min(high)
+mi_l = min(low)
+ma_h = max(high)
+ma_l = max(low)
+q_h = [round(q, 3) for q in statistics.quantiles(high, n=4)]
+q_l = [round(q, 3) for q in statistics.quantiles(low, n=4)]
+
+err_mean_h = statistics.mean(errm)
+err_sd_h = statistics.stdev(errm)
+err_mi_h = min(errm)
+err_ma_h = max(errm)
+err_q_h = [round(q, 5) for q in statistics.quantiles(errm, n=4)]
+
+print('Accepted data:\n')
+print('Inlier ratio statistic:')
+print('Min:', mi_h, 'Max:', ma_h, 'Mean:', mean_h, 'Median:', q_h[1])
+print('StDev:', sd_h, 'q25:', q_h[0], 'q75:', q_h[2])
+print('\nReprojection error statistic:')
+print('Min:', err_mi_h, 'Max:', err_ma_h, 'Mean:', err_mean_h, 'Median:', err_q_h[1])
+print('StDev:', err_sd_h, 'q25:', err_q_h[0], 'q75:', err_q_h[2])
+print('\n\nDismissed data:')
+print('Inlier ratio statistic:')
+print('Min:', mi_l, 'Max:', ma_l, 'Mean:', mean_l, 'Median:', q_l[1])
+print('StDev:', sd_l, 'q25:', q_l[0], 'q75:', q_l[2])
+
+print('Reprojection error statistic:\n')
