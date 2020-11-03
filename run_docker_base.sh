2a576b778bb2bf52ae73c693d44951107d59e4dc
Josef Maier <josefmaierfl@users.noreply.github.com>
2020-11-03 12:39:55 +0100

    Added option to specify full results dir

--- run_docker_base.sh
+++ run_docker_base.sh
@@ -18,10 +18,21 @@ else
   echo "First argument must be shutdown or live"
   exit 1
 fi
+shift 1
 
-SECOND_ARG="$2"
+USE_FULL_PATH=0
+if [ "$1" == "fullp" ]; then
+  USE_FULL_PATH=1
+  shift 1
+fi
+
+SECOND_ARG="$1"
 if [ "${SECOND_ARG}" == "RESDIR" ]; then
-  RES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )/$3"
+  if [ ${USE_FULL_PATH} -eq 0 ]; then
+    RES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )/$2"
+  else
+    RES_DIR="$2"
+  fi
   if [ -d ${RES_DIR} ]; then
     shift 2
   else
@@ -37,7 +48,7 @@ fi
 # -c $(echo "${@:2}")
 xhost +local:
 #docker run -v `pwd`/images:/app/images:ro -v `pwd`/py_test_scripts:/app/py_test_scripts -v ${RES_DIR}:/app/results -v ${RES_SV_DIR}:/app/res_save_compressed -it -v /tmp/.X11-unix/:/tmp/.X11-unix:ro ac_test_package:1.0 /bin/bash
-docker run -v `pwd`/images:/app/images:ro -v `pwd`/py_test_scripts:/app/py_test_scripts -v ${RES_DIR}:/app/results -v ${RES_SV_DIR}:/app/res_save_compressed -it -v /tmp/.X11-unix/:/tmp/.X11-unix:ro ac_test_package:1.0 /app/start_testing.sh "${@:2}"
+docker run -v `pwd`/images:/app/images:ro -v `pwd`/py_test_scripts:/app/py_test_scripts -v ${RES_DIR}:/app/results -v ${RES_SV_DIR}:/app/res_save_compressed -it -v /tmp/.X11-unix/:/tmp/.X11-unix:ro ac_test_package:1.0 /app/start_testing.sh "$@"
 
 # Shut down if asked for
 #if [ $# -ne 0 ]; then
